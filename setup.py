11344784b4eaec7f8c835da5baa97483ce0770a0
zhujianfeng <zjf014@gmail.com>
2023-09-05 20:44:43 +0800

    整合minio读写

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zjf014/hydro-opendata',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
